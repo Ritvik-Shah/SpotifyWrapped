78154ab81c7bd81d2eca3ff5afa1685a95b37802
shayangit20 <m.shayanwaqar.03@gmail.com>
2024-04-04 14:22:09 -0400

    login page

---
+++ app/src/main/java/com/example/spotifywrapped/ui/login/LoginFragment.java
@@ -0,0 +1,28 @@
+package com.example.spotifywrapped.ui.login;
+
+import android.app.Activity;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.fragment.app.Fragment;
+
+public class LoginFragment extends Fragment {
+    private EditText username, pwd;
+    private Button loginBtn;
+
+
+
+
+
+
+
+}
