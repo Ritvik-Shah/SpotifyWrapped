67c77544d2cbe29725280730f4cdb1a0b45ae329
shayangit20 <m.shayanwaqar.03@gmail.com>
2024-04-09 14:28:13 -0400

    login page - updated strings error

--- app/src/main/java/com/example/spotifywrapped/ui/login/LoginFragment.java
+++ app/src/main/java/com/example/spotifywrapped/ui/login/LoginFragment.java
@@ -3,8 +3,10 @@ package com.example.spotifywrapped.ui.login;
 import android.app.Activity;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
 
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -13,12 +15,31 @@ import android.widget.Toast;
 import android.widget.Button;
 import android.widget.EditText;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.example.spotifywrapped.databinding.FragmentUpdateLoginBinding;
 
 public class LoginFragment extends Fragment {
     private EditText username, pwd;
     private Button loginBtn;
 
+    private FragmentUpdateLoginBinding binding;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+
+        binding = FragmentUpdateLoginBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+        return root;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
 
 
 
