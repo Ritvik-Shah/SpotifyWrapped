e60e647db5fd18ad14e801bb47f3356dd8d7229f
Mukund <mukund.chidu@gmail.com>
2024-04-20 22:00:29 -0400

    add scroll and holiday color changes

--- app/src/main/java/com/example/spotifywrapped/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/spotifywrapped/ui/home/HomeFragment.java
@@ -1,21 +1,27 @@
 package com.example.spotifywrapped.ui.home;
 
 import android.content.Intent;
+import android.graphics.drawable.ColorDrawable;
 import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.example.spotifywrapped.MainActivity;
+import com.example.spotifywrapped.R;
 import com.example.spotifywrapped.User;
 import com.example.spotifywrapped.databinding.FragmentHomeBinding;
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -36,6 +42,7 @@ import org.json.JSONObject;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 import java.util.Random;
 
@@ -109,6 +116,48 @@ public class HomeFragment extends Fragment {
                 //getTracks();
             }
         });
+
+        Window window = requireActivity().getWindow();
+        ActionBar actionBar = ((AppCompatActivity) requireActivity()).getSupportActionBar();
+        Calendar calendar = Calendar.getInstance();
+        int month = calendar.get(Calendar.MONTH) + 1; // Months are 0-based
+        int day = calendar.get(Calendar.DAY_OF_MONTH);
+
+
+// Set background color based on the date
+        if (month == 10 && day == 31) {
+            // October 31st - Orange
+            //sendNotification("Halloween Edition", "Check out the new colors for Halloween!!!");
+            window.setStatusBarColor(getResources().getColor(R.color.black));
+            if (actionBar != null) {
+                actionBar.setBackgroundDrawable(new ColorDrawable(ContextCompat.getColor(requireContext(), R.color.orange)));
+            }
+        } else if (month == 12 && day == 25) {
+            // December 25th - Red
+            window.setStatusBarColor(getResources().getColor(R.color.green));
+            if (actionBar != null) {
+                actionBar.setBackgroundDrawable(new ColorDrawable(ContextCompat.getColor(requireContext(), R.color.red)));
+            }
+        } else if (month == 1 && day == 1) {
+            // January 1st - Gold
+            window.setStatusBarColor(getResources().getColor(R.color.black));
+            if (actionBar != null) {
+                actionBar.setBackgroundDrawable(new ColorDrawable(ContextCompat.getColor(requireContext(), R.color.gold)));
+            }
+        } else if (month == 7 && day == 4) {
+            // July 4th - Blue
+            window.setStatusBarColor(getResources().getColor(R.color.blue));
+            if (actionBar != null) {
+                actionBar.setBackgroundDrawable(new ColorDrawable(ContextCompat.getColor(requireContext(), R.color.blue)));
+            }
+        } else {
+            // Default background color
+            window.setStatusBarColor(getResources().getColor(R.color.black));
+            if (actionBar != null) {
+                actionBar.setBackgroundDrawable(new ColorDrawable(ContextCompat.getColor(requireContext(), R.color.purple_700)));
+            }
+        }
+
         return root;
     }
 
